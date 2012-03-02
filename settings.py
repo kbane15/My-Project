e00cf2fb7d4ee14a01069d084291fce6ed5a284e
Katie Bane <foxx9115@cox.net>
2012-03-02 10:51:51 -0600

    done

--- settings.py
+++ settings.py
@@ -103,7 +103,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'myproject.urls'
 
 TEMPLATE_DIRS = (
-    'home/myproject/templates',
+    '/home/katie/myproject/templates/',
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
